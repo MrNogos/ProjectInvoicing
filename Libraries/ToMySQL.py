2ca6126222f40a187f4a6961762233e6ec651411
JavierPerrusquia <62264198+MrNogos@users.noreply.github.com>
2022-08-05 01:26:56 -0500

    Add files via upload
    Added columns for MYSQL

--- Libraries/ToMySQL.py
+++ Libraries/ToMySQL.py
@@ -10,6 +10,38 @@ mycursor = db.cursor()
 
 mycursor.execute("CREATE DATABASE IF NOT EXISTS DBGP;")
 mycursor.execute("USE DBGP;")
-mycursor.execute("CREATE TABLE IF NOT EXISTS FACTURAS_INTERNAS")
+Table = ["Serie VARCHAR(1)",
+    "Version VARCHAR(3)",
+    "Rfc VARCHAR(12)",
+    "RegimenFiscal INT",
+    "UsoCFDI VARCHAR(4)"
+    "ClaveProdServ INT"
+    'Cantidad INT'
+    "ClaveUnidad VARCHAR(4)"
+    "ValorUnitario INT"
+    "Importe INT"
+    "Base INT"
+    "Impuesto INT"
+    "TipoFactor VARCHAR(6)"
+    "TasaOCuota INT"
+    "TotalImpuestosTrasladados INT"
+    "Rfc CHAR(12)"
+    "Nombre CHAR(50)"
+    "RegimenFiscal (INT)"
+    "Rfc VARCHAR(12)"
+    "Nombre VARCHAR(50)"
+    "UsoCFDI VARCHAR(4)"
+    "Descripcion	MEDIUMTEXT(500)"
+    "ValorUnitario int"
+    "Importe	int"
+    "UUID VARchar(36)"
+    "FechaTimbrado DATETIME(3)"
+
+
+]
+
+#mycursor.execute("CREATE TABLE IF NOT EXISTS Facturas_Internas(MetodoPago varchar(3), TipoDeComprobante varchar(1), Total INT, Moneda VARCHAR(5), SubTotal INT, FormaPago INT, Folio INT)")
+
+
 
 
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+from telnetlib import PRAGMA_HEARTBEAT
 import Libraries.excel as excel
 import Libraries.XML as XML
 
-#example =["APR861231KL3",	"ARTEFACTOS DE PRECISION, S.A. DE C.V.",	"26111707",	
-#"VENTA DE REFACCIONES Y BATERIA PARA UPS SE INCLUYE SERVIC IO Y MANTENIMIENTO. P.O. O: P081964-00", "$3,373.32", "$3,913.05",
-#	"63D68DB5-2D47-4D08-AB14-92BBE24A5516",	"A",	"964",	"03-05-2022 14:03",	"Vigente",	"Mayo"]
-
-path = excel.get_excel_path()
+#path = excel.get_excel_path()
 
 dict_from_XML = XML.startXML()
-print("hi")
-a = excel.scrap_dictionary(dict_from_XML)
 
+#a = excel.scrap_dictionary(dict_from_XML)
+#print(dict_from_XML.values())
+for i in dict_from_XML["Comprobane"]:
+    print(i+ ": " + str(dict_from_XML["Comprobane"][i].values()))
+
+for i in dict_from_XML.keys():
+    print(i + ":")
+    for j in dict_from_XML[i].keys():
+        print("     "+j + ": "+ str(dict_from_XML[i][j]))
+#print(dict_from_XML["Comprobane"].values())
 #print("From Dict\n"+a[9]+"\nDict lenght: "+str(len(a)))
 #print("From Example\n"+example[9]+"\nExample lenght: "+str(len(example)))
-excel.store_invoice(path, a)
+#excel.store_invoice(path, a)
+
+print(len(dict_from_XML["Complementos"]["UUID"]))

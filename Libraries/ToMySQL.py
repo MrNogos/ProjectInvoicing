7f623ad05dd2837ab455e9430e6283ff382bef1a
JavierPerrusquia <62264198+MrNogos@users.noreply.github.com>
2022-08-08 22:24:35 -0500

    Update ToMySQL.py
    Edit access MySQL

--- Libraries/ToMySQL.py
+++ Libraries/ToMySQL.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="mynewpassword"
+    host="",
+    user="",
+    passwd=""
 )
 
 mycursor = db.cursor()
